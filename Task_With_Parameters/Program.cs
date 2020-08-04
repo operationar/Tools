f4ae563e2d86663af713b1d2f18eeb07ff9860ef
春峰 <wang.chunfeng@outlook.com>
2020-08-04 11:58:15 +0800

    添加项目文件。

---
+++ Task_With_Parameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_With_Parameters
+{
+    class Program
+    {
+        static  string func(object s)
+        {
+            Console.WriteLine(s.ToString());
+            return s.ToString();
+        }
+            static void Main(string[] args)
+        {
+            int i = 0;i++;
+            List<Task<string>> taskList = new List<Task<string>>();
+            for (int j = 0; j < 1000; j++)
+            {
+                taskList.Add(Task.Factory.StartNew(func, j, TaskCreationOptions.LongRunning));
+                
+               //taskList.Add( Task.Run<string>(() =>  func(j) ));         
+            }
+            Task.WaitAll(taskList.ToArray());
+            foreach (var item in taskList)
+            {
+                Console.WriteLine(item.Result);
+            }
+            i++;
+            Console.ReadKey();
+        }
+    }
+}
